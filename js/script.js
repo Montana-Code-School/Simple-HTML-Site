e7d5e5af5976269675caf3a3cc3ce90837187f9c
aceslowman <aceslowman@gmail.com>
2017-03-22 15:39:14 -0600

    Moved the script to a more appropriate location, also added a placeholder image of my cat.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*
+  Below, we are simply using JavaScript to trigger a console
+  message when we click within the main container div.
+*/
+
+document.getElementById('container').onclick = function(){
+  console.log("You've triggered an onclick event!");
+}
